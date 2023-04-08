a2082aa8ecc061763d4f761d44e0580f13300cf2
PietroColaguori <pietro.colaguori3889@gmail.com>
2023-04-08 16:41:51 +0200

    Aggiunto salvataggio nome utente

--- Access.py
+++ Access.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore, storage
 import re
 
 # Set icon
-st.set_page_config(page_title="MySlack", page_icon="https://cdn3.iconfinder.com/data/icons/social-network-30/512/social-08-512.png")
+st.set_page_config(page_title="MySlack", page_icon="🔐")
 
 # Get the client
 if not firebase_admin._apps:
@@ -42,9 +42,9 @@ def admin_already_registered(username):
             if(doc_dict["isAdmin"] == "True"): return True
     return False
 
-# Manage logged in and admin status
+# Manage session state informations ========================
 if 'logged_in' not in st.session_state:
-    st.session_state['logged_in'] = [False, False]
+    st.session_state['logged_in'] = [False, False, "not_set"]
 
 def logged_in():
     if(st.session_state.logged_in[0] == False):
@@ -62,6 +62,7 @@ def logged_in():
                     st.session_state.logged_in[0] = True
                     if(doc_dict["isAdmin"] == "True"): 
                         st.session_state.logged_in[1] = True
+                    st.session_state.logged_in[2] = username
             if(not st.session_state.logged_in): st.error("Username or password not correct")
     return st.session_state['logged_in']
      
@@ -123,3 +124,4 @@ if(not st.session_state.logged_in[0]):
 
 else:
     st.header("You are already logged in!")
+    st.subheader("To sign in again press CTRL+R")
